f977257568878533a1beede30ac6b2d63844b98e
pince traders <rspclibrary@gmail.com>
2026-05-29 17:04:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/market", async (req, res) => {
+app.get("/", async (req, res) => {
 
 let btc = "Loading...";
 let eth = "Loading...";
